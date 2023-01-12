2b3e3e0c492c76a6367d60bb221274027feea2cf
Oksana Yerep <114570807+Oksana-y@users.noreply.github.com>
2023-01-12 20:50:16 +0200

    Working witn flags

--- src/index.js
+++ src/index.js
@@ -11,83 +11,70 @@ const refs = {
 };
 
 function countryMarkup(countries) {
-  refs.infoBox.innerHTML = countries.map(({name,capital,population,flags,languages}) => {
-    return`
-     <img src= ${flags.svg} style="width:50px; height:50px/>
+  const countryCard = countries
+    .map(({ name, capital, population, flags, languages }) => {
+      return `
+    <img src="${flags.svg}" alt="#" style="width:50px;height:30px;"/>
     <h2>${name.common}</h2>
     <p>Capital: ${capital}</p>
     <p>Population: ${population}</p>
-    <p>Languages: ${Object.values(languages).join(",")}</p>>
-    `
+    <p>Languages: ${Object.values(languages).join(',')}</p>
+    `;
     })
     .join('');
-// console.log(countryCard);
-  // refs.infoBox.innerHTML = '';
-  // refs.infoBox.insertAdjacentHTML('beforeend', countryCard);
+
+  refs.infoBox.innerHTML = '';
+  refs.list.innerHTML = '';
+  refs.infoBox.insertAdjacentHTML('beforeend', countryCard);
 }
 
 function countriesMarkup(countries) {
-  refs.list.innerHTML = countries
+  const countriesList = countries
     .map(({ name, flags }) => {
       return `
-    <li><img src= ${flags.svg} style="width:50px; height:50px/>
+    <li><img src="${flags.svg}" alt="#" style="width:50px;height:30px;"/>
     <h2>${name.common}</h2>
     </li>
     `;
     })
     .join('');
-  // refs.list.innerHTML = '';
-  // refs.list.insertAdjacentHTML('beforeend', countriesList);
-  // console.log(countriesList);
+  refs.list.innerHTML = '';
+  refs.infoBox.innerHTML = '';
+  refs.list.insertAdjacentHTML('beforeend', countriesList);
 }
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
   e.preventDefault();
-  
 
   const name = e.target.value.trim();
-  
+  if (name === '') {
+    refs.infoBox.innerHTML = '';
+    refs.list.innerHTML = '';
+    return;
+  }
 
   const result = fetchCountries(name);
 
   result
     .then(countries => countryCardMaker(countries))
     .catch(error => Notiflix.Notify.failure('error 404'));
-
-  if (name === '') {
-    refs.infoBox.innerHTML = '';
-    refs.list.innerHTML = '';
-    return;
-  }
 }
 
 function countryCardMaker(countries) {
   if (countries.length === 1) {
     countryMarkup(countries);
-    
   } else if (countries.length >= 2 && countries.length <= 10) {
     countriesMarkup(countries);
-    
   } else if (countries.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
+  } else if (countries.length === 0) {
+    refs.infoBox.innerHTML = '';
+    refs.list.innerHTML = '';
   } else {
     Notiflix.Notify.failure('Oops, there is no country with that name');
   }
 }
-
-// const result = fetchCountries(name);
-//   result.then(countries => { console.log(countries)
-//     .catch(error => Notiflix.Notify.failure('error 404'));
-
-// refs.input.addEventListener('input', lodash(onInput, DEBOUNCE_DELAY));
-
-// refs.list.insertAdjacentHTML('beforeend', countriesList);
-//     console.log(countriesList);
-//     refs.list.innerHTML = "";
-
-// refs.infoBox.insertAdjacentHTML('beforeend', countryCard);
-//     refs.infoBox.innerHTML = "";
